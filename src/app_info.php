164cc7054f8bd36f0139e31ef2c91d6cc09d596d
Juan Miguel Medina Prieto <juanmiguelmedinaprieto@yahoo.es>
2020-12-21 11:18:33 +0000

    feat: add script to make data fetching easier on backend

---
+++ src/app_info.php
@@ -0,0 +1,10 @@
+<?php
+
+$env = parse_ini_file("/etc/.env");
+
+$response = array_filter($env, function ($key) {
+    return preg_match("/^DB_/", $key) === 0;
+}, ARRAY_FILTER_USE_KEY);
+
+header("Content-Type: application/json");
+echo json_encode($response);
