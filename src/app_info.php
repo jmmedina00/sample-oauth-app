c5293eb03f431e732d825fb28d1831f52e209c62
Juan Miguel Medina Prieto <juanmiguelmedinaprieto@yahoo.es>
2020-12-21 13:31:04 +0000

    feat(app_info): exclude secrets from returned data

--- src/app_info.php
+++ src/app_info.php
@@ -3,7 +3,8 @@
 $env = parse_ini_file("/etc/.env");
 
 $response = array_filter($env, function ($key) {
-    return preg_match("/^DB_/", $key) === 0;
+    return preg_match("/^DB_/", $key) === 0 
+    && preg_match("/_SECRET$/", $key) === 0;
 }, ARRAY_FILTER_USE_KEY);
 
 header("Content-Type: application/json");
