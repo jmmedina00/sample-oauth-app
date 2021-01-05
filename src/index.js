e16462a20b2f239dc06469398492ee6f7cfbcb73
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-01-05 14:50:49 +0100

    refactor(index): calculate all links and stuff with OAuth object

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const oauthData = {
         responseType: "code"
     },
     dropbox: {
-        baseLink: "https://www.dropbox.com/oauth2/authorize?",,
+        baseLink: "https://www.dropbox.com/oauth2/authorize?",
         scope: "account_info.read",
         responseType: "code"
     },
@@ -28,55 +28,19 @@ const oauthData = {
 }
 
 window.onload = async () => {
-    const googleAuth = document.querySelector("#googleAuth");
-    const gitHubAuth = document.querySelector("#githubAuth");
-    const gitlabAuth = document.querySelector("#gitlabAuth");
-    const dropboxAuth = document.querySelector("#dropboxAuth");
-    const redditAuth = document.querySelector("#redditAuth");
-
     const apiResponse = await fetch("/app_info.php");
-    const {
-        GOOGLE_CLIENT_ID, GITHUB_CLIENT_ID, GITLAB_CLIENT_ID,
-        DROPBOX_CLIENT_ID, REDDIT_CLIENT_ID
-    } = await apiResponse.json();
+    const clientIds = await apiResponse.json();
 
-    const googleScope = `scope=${encodeURIComponent(google.scope)}`
-    const googleRedirectUri = `redirect_uri=${encodeURIComponent(google.redirectUri)}`;
-    const googleClientId = `client_id=${GOOGLE_CLIENT_ID}`;
+    for (const [service, {baseLink, scope, responseType}] of Object.entries(oauthData)) {
+        const clientId = clientIds[`${service.toUpperCase()}_CLIENT_ID`];
+        const authElement = document.querySelector(`#${service}Auth`);
 
-    const githubClientId = `client_id=${GITHUB_CLIENT_ID}`;
-    const githubRedirectUri = `redirect_uri=${encodeURIComponent(github.redirectUri)}`;
-
-    const gitlabClientId = `client_id=${GITLAB_CLIENT_ID}`;
-    const gitlabRedirectUri = `redirect_uri=${encodeURIComponent(gitlab.redirectUri)}`;
-
-    const dropboxClientId = `client_id=${DROPBOX_CLIENT_ID}`;
-    const dropboxRedirectUri = `redirect_uri=${encodeURIComponent(dropbox.redirectUri)}`;
-
-    const redditClientId = `client_id=${REDDIT_CLIENT_ID}`;
-    const redditRedirectUri = `redirect_uri=${encodeURIComponent(reddit.redirectUri)}`;
-
-    googleAuth.setAttribute(
-        "href",
-        `${google.baseLink}${googleScope}&${googleRedirectUri}&${googleClientId}&response_type=code`
-    );
-    gitHubAuth.setAttribute(
-        "href",
-        `${github.baseLink}${githubClientId}&${githubRedirectUri}&scope=user:email`
-    );
-
-    gitlabAuth.setAttribute(
-        "href",
-        `${gitlab.baseLink}${gitlabClientId}&${gitlabRedirectUri}&response_type=code&scope=read_user`
-    );
-
-    dropboxAuth.setAttribute(
-        "href",
-        `${dropbox.baseLink}${dropboxClientId}&${dropboxRedirectUri}&response_type=code&scope=account_info.read`
-    );
-
-    redditAuth.setAttribute(
-        "href",
-        `${reddit.baseLink}${redditClientId}&${redditRedirectUri}&response_type=code&scope=identity&duration=temporary&state=lipsum`
-    );
+        const scopeString = `scope=${encodeURIComponent(scope)}`;
+        const responseTypeString = responseType ? `&response_type=${responseType}` : "";
+        const redirectUri = `&redirect_uri=${encodeURIComponent(BASE_REDIRECT + service + ".php")}`;
+        authElement.setAttribute(
+            "href",
+            `${baseLink}${scopeString}${responseTypeString}${redirectUri}&client_id=${clientId}`
+        );
+    }
 }
