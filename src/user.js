bc21cc33dabaa90a00bcfcd0019113c18bc9b6d5
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2021-01-09 14:06:43 +0100

    feat(user): use user endpoint to fill in the view

--- src/user.js
+++ src/user.js
@@ -1,6 +1,13 @@
 window.onload = async () => {
+  const keys = ["name", "email", "accesses"]
   const params = (new URL(document.location)).searchParams;
   const userId = params.get("id");
 
-  console.log(userId);
+  const apiResponse = await fetch(`/get_user.php?id=${userId}`);
+  const user = await apiResponse.json();
+
+  for (const key of keys) {
+    const value = user[key];
+    document.querySelector(`#${key}`).innerHTML = value;
+  }
 }
